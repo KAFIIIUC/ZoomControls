7fa58878469dcfbc87ceba71ce1e0d51840fa4b2
Shamim kafi <shamimunbindelowarkafi@gmail.com>
2018-08-01 00:22:41 +0600

    Initial commit

---
+++ app/src/main/java/com/kafi/zoomcontrols/MainActivity.java
@@ -0,0 +1,52 @@
+package com.kafi.zoomcontrols;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.ZoomControls;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    ZoomControls zoomControls;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageViewId);
+        zoomControls = findViewById(R.id.zoomControlId);
+
+        zoomControls.setOnZoomInClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                float x = imageView.getScaleX();
+                float y = imageView.getScaleY();
+
+                imageView.setScaleX((float)x+1);
+                imageView.setScaleY((float)y+1);
+
+                Toast.makeText(MainActivity.this, "Zoom In", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        zoomControls.setOnZoomOutClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                float x = imageView.getScaleX();
+                float y = imageView.getScaleY();
+
+                if (x>1 && y>1){
+                    imageView.setScaleX((float)x-1);
+                    imageView.setScaleY((float)y-1);
+                }
+                Toast.makeText(MainActivity.this, "Zoom Out", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
